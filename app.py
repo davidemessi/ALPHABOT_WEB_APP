f96b915fb242e04f923c4e576e9a6cb307b49f1f
Davide <davide.messi@itiscuneo.eu>
2026-01-29 10:33:56 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import time
 ab = AlphaBot()
 ab.stop()
 
+VELOCITA_FORTE = 100     # avanti / indietro
+VELOCITA_PIANO = 40
+VELOCITA_CURVA = 40     # destra / sinistra
+
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -13,12 +17,10 @@ def index():
 
     listaComandiDB = ()
     
-    # prendo il comando selezionato
     comandoSelezionato = None
     if request.method == "POST":
         comandoSelezionato = list(request.form.keys())[0]
 
-    # prendo i comandi presenti nel db
     con = sqlite3.connect('./alphaBot_DB.db')
     cur = con.cursor()
     cur.execute("SELECT nome_comando FROM Comandi")
@@ -26,28 +28,44 @@ def index():
     listaComandiDB = [riga[0] for riga in righe]
     con.close()
 
-
     print(listaComandiDB)
-    print(comandoSelezionato.lower())
+    print(comandoSelezionato)
 
     if request.method == "POST":
-        if comandoSelezionato == "Avanti":
+
+        if comandoSelezionato == "AvantiForte":
             print(comandoSelezionato)
+            ab.setMotor(VELOCITA_FORTE, VELOCITA_FORTE)
+            ab.forward()
+            return render_template("index.html")
+        
+        elif comandoSelezionato == "AvantiPiano":
+            print(comandoSelezionato)
+            ab.setMotor(-VELOCITA_PIANO, -VELOCITA_PIANO)
             ab.forward()
             return render_template("index.html")
 
-        elif comandoSelezionato == "Indietro":
+        elif comandoSelezionato == "IndietroForte":
             print(comandoSelezionato)
+            ab.setMotor(-VELOCITA_FORTE, -VELOCITA_FORTE)
+            ab.backward()
+            return render_template("index.html")
+        
+        elif comandoSelezionato == "IndietroPiano":
+            print(comandoSelezionato)
+            ab.setMotor(-VELOCITA_PIANO, -VELOCITA_PIANO)
             ab.backward()
             return render_template("index.html")
 
         elif comandoSelezionato == "Destra":
             print(comandoSelezionato)
+            ab.setMotor(VELOCITA_CURVA, VELOCITA_CURVA)
             ab.right()
             return render_template("index.html")
 
         elif comandoSelezionato == "Sinistra":
             print(comandoSelezionato)
+            ab.setMotor(VELOCITA_CURVA, VELOCITA_CURVA)
             ab.left()
             return render_template("index.html")
 
@@ -65,33 +83,40 @@ def index():
                 (comandoSelezionato.lower(),)
             )
             righe = cur.fetchall()
-            print(f"Righe: {righe}")
             con.close()
 
             movimenti, tempi = righe[0]
             lista_mov = [m.strip() for m in movimenti.split(',')]
             lista_tempi = [t.strip() for t in tempi.split(',')]
 
-            for mov, tempi in zip(lista_mov, lista_tempi):
+            for mov, t in zip(lista_mov, lista_tempi):
+
                 if mov == 'avanti':
+                    ab.setMotor(VELOCITA_FORTE, VELOCITA_FORTE)
                     ab.forward()
-                    time.sleep(float(tempi))
-                if mov == 'indietro':
+                    time.sleep(float(t))
+
+                elif mov == 'indietro':
+                    ab.setMotor(-VELOCITA_FORTE, -VELOCITA_FORTE)
                     ab.backward()
-                    time.sleep(float(tempi))
-                if mov == 'destra':
+                    time.sleep(float(t))
+
+                elif mov == 'destra':
+                    ab.setMotor(VELOCITA_CURVA, VELOCITA_CURVA)
                     ab.right()
-                    time.sleep(float(tempi))
-                if mov == 'sinistra':
+                    time.sleep(float(t))
+
+                elif mov == 'sinistra':
+                    ab.setMotor(VELOCITA_CURVA, VELOCITA_CURVA)
                     ab.left()
-                    time.sleep(float(tempi))
-                if mov == 'aspetta':
+                    time.sleep(float(t))
+
+                elif mov == 'stop':
                     ab.stop()
-                    time.sleep(float(tempi))
 
             return render_template("index.html")
 
-    elif request.method == "GET":
-        return render_template("index.html")
+    return render_template("index.html")
+
 
 app.run(debug=False, host="0.0.0.0")
